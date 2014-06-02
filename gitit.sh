95b024eb5de7bfa0d0d5e3c06e0088b7723adc07
Jeffrey David Johnson <jefdaj@arachno.phobia.no-ip.info>
2014-06-01 23:52:33 -0700

    tried to get gitit to find plugins, but failed

--- gitit.sh
+++ gitit.sh
@@ -4,15 +4,33 @@
 # and less error-prone. NOT a substitute for knowing
 # what each of these commands does!
 
+
+############
+# variables
+############
+
 ROOTDIR="$(cd `dirname "$0"` && pwd)"
-PATH="$ROOTDIR/.cabal-sandbox/bin:$PATH"
-TMPDIR="$ROOTDIR/.cabal-sandbox/tmp"
+BINDIR="$ROOTDIR/.cabal-sandbox/bin"
+TESTWIKI="$ROOTDIR/testwiki"
+
+# this is needeed because if your distro doesn't give /tmp execute permissions
+# you have to compile somewhere else, or there will be all sorts of weird errors
+CABAL_SANDBOX_TMPDIR="$ROOTDIR/.cabal-sandbox/tmp"
+
+# and this is needed because ghc doesn't understand cabal sandboxes yet
+# see http://mappend.net/posts/ghc-and-cabal-sandbox-playing-ni
+CABAL_SANDBOX_PKGPATH="$(cabal sandbox hc-pkg list | grep \: | tac | sed 's/://' | paste -d: - -)"
+
+
+############
+# functions
+############
 
 prepare_repo() {
   cd "$ROOTDIR"
   cabal update
   cabal sandbox init
-  [[ -d "$TMPDIR" ]] || mkdir "$TMPDIR"
+  [[ -d "$CABAL_SANDBOX_TMPDIR" ]] || mkdir "$CABAL_SANDBOX_TMPDIR"
 }
 
 build_gitit() {
@@ -24,11 +42,19 @@ test_gitit() {
   cd "$ROOTDIR/testwiki/wikidata"
   [[ -d .git ]] || git init
   git add . && git commit -m 'make sure test pages will show up'
-  cd "$ROOTDIR/testwiki"
-  gitit --config-file testwiki.conf
+  cd "$ROOTDIR"
+  CABAL_SANDBOX_CONFIG="$ROOTDIR/cabal.sandbox.config" \
+    GHC_PACKAGE_PATH="$CABAL_SANDBOX_PKGPATH" \
+    PATH="$BINDIR":$PATH \
+    TMPDIR="$CABAL_SANDBOX_TMPDIR" \
+    gitit --config-file "$TESTWIKI/testwiki.conf"
 }
 
-set -e
+
+#######
+# main
+#######
+
 for arg in "$@"; do
   case "$arg" in
     'prep' ) prepare_repo ;;
