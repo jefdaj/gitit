74e1eb0a47df9ed3220ff3615bdf2898e46862fa
Jeffrey David Johnson <jefdaj@protonmail.ch>
2014-09-30 22:43:03 -0700

    added link2path to Csv.hs

--- plugins/Csv.hs
+++ plugins/Csv.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 import Network.Gitit.Interface
 
 -- TODO add this to Network.Gitit.Interface
-import Network.Gitit.Plugin.External (link2path)
+-- import Network.Gitit.Plugin.External (link2path)
 
 import System.Directory
 import System.FilePath.Posix
@@ -42,6 +42,18 @@ table c t = Table c a w h r
 -- extracts a caption from block attributes
 caption :: [(String, String)] -> [Inline]
 caption as = map Str $ maybeToList $ lookup "caption" as
+
+-- takes an absolute or relative gitit link
+-- and makes it into a file path relative to the running program
+link2path :: String -> PluginM FilePath
+link2path lnk = do
+  cfg <- askConfig
+  pfp <- askFile
+  -- if it starts with "/", the link is relative to the repository
+  -- otherwise, it's relative to the requested page
+  case isPrefixOf "/" lnk of
+    True  -> return $ joinPath [repositoryPath cfg, dropWhile (== '/') lnk]
+    False -> return $ joinPath [takeDirectory pfp, lnk]
 
 -- gets block attributes and body text, and
 -- reads a file to replace the text if needed
