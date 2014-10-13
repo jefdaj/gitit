7fdf61bfb79737547b55b6e1a65fecd9b253acd4
Jeffrey David Johnson <jefdaj@protonmail.ch>
2014-10-12 17:27:51 -0700

    added perl example

--- plugins/perlexample.pl
+++ plugins/perlexample.pl
@@ -2,3 +2,20 @@
 
 # Example of how to write an external PageTransform plugin in Perl.
 
+use strict;
+
+print "Hello from Perl!";
+print "<br/>";
+print "<br/>";
+
+print "Your script recieved these args:";
+print "<br/>";
+print "@ARGV";
+print "<br/>";
+print "<br/>";
+
+print "And this text from stdin:";
+print "<br/>";
+print <STDIN>;
+print "<br/>";
+print "<br/>";
