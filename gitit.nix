886f44a0b5d79cb9f994eb31341130d6a5e8cdfb
Jeffrey David Johnson <jefdaj@protonmail.ch>
2016-02-23 07:58:08 -0800

    update gitit.nix for 0.12.1

--- gitit.nix
+++ gitit.nix
@@ -10,8 +10,8 @@
 }:
 mkDerivation {
   pname = "gitit";
-  version = "0.12.0.1";
-  src = ./.;
+  version = "0.12.1";
+  sha256 = "0wvxynb0bkbbnn9m2m2ivlqjzr2gj9ylp6z5xxpavqakfm70kky2";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
@@ -21,7 +21,8 @@ mkDerivation {
     HTTP http-client-tls http-conduit json mtl network network-uri
     old-locale old-time pandoc pandoc-types parsec pretty process
     random recaptcha safe SHA split syb tagsoup text time uri url
-    utf8-string uuid xhtml xml xss-sanitize zlib pandoc-citeproc
+    utf8-string uuid xhtml xml xss-sanitize zlib
+    pandoc-citeproc
   ];
   executableHaskellDepends = [
     base bytestring directory filepath hslogger HTTP mtl network
