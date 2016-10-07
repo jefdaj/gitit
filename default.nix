6212e77b70a73bd8927f679d9982e1fdd77b4490
Jeffrey David Johnson <jefdaj@protonmail.ch>
2016-10-07 13:51:30 -0400

    fixed nix dot dependency

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7103" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gitit.nix { }
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gitit.nix { graphviz = nixpkgs.pkgs.graphviz; }
