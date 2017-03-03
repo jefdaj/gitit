fc8d3a4ccfd6ddcf93dd858544bfa8ee7dc4128d
Jeffrey David Johnson <jefdaj@protonmail.ch>
2017-03-02 23:31:22 -0800

    got stack working, but broke nix

--- shell.nix
+++ shell.nix
@@ -1,2 +1,26 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+# TODO is nix required to get the runtime dependencies?
+#      if so, probably want both stack *inside* nix-shell!
+#      buildStackProject seems to support passthrough of all args,
+#      so that should be totally doable
+
+{ghc}:
+with (import <nixpkgs> {});
+
+let
+  myTexLive = texlive.combine {
+    inherit (texlive) scheme-small collection-xetex;
+  };
+
+in haskell.lib.buildStackProject {
+  inherit ghc;
+  name = "gititEnv";
+  buildInputs = [ zlib myTexLive ]; # TODO propogate i guess?
+
+  # extra depdendencies for plugins
+  # buildDepends = [ graphviz myTexLive makeWrapper ];
+  # postInstall = ''
+  #   wrapProgram $out/bin/gitit \
+  #     --prefix PATH : "${graphviz}/bin:${myTexLive}/bin"
+  # '';
+
+}
