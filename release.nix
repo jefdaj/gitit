0ac72e555186a1d9c798740ac2b90facb0d2b7c6
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-10-17 08:40:53 -0700

    wip: start new nix files

---
+++ release.nix
@@ -0,0 +1,30 @@
+let
+  sources = import ./nix/sources.nix;
+in
+{ compiler ? "ghc884"
+, pkgs ? import sources.nixpkgs { }
+}:
+let
+  inherit (pkgs.haskell.lib) markUnbroken;
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hpNew: hpOld: {
+      niv   = import sources.niv {};
+      uri   = markUnbroken hpOld.uri;
+      gitit = hpNew.callCabal2nix "gitit" ./. {};
+    };
+  };
+  project = haskellPackages.gitit;
+in
+{
+  project = project;
+  shell = haskellPackages.shellFor {
+    packages = p: with p; [
+      project
+    ];
+    buildInputs = with haskellPackages; [
+      ghcid
+      hlint
+    ];
+    withHoogle = true;
+  };
+}
