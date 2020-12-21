e5dff193e1c272005fbf59a3f89902e154a64161
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-12-21 12:25:52 -0800

    add texlive to pandoc, but printing still broken

--- release.nix
+++ release.nix
@@ -16,6 +16,12 @@ let
       niv = import sources.niv {};
       gitit = hpNew.callCabal2nix "gitit" ./. {};
 
+      pandoc = overrideCabal (dontCheck (hpNew.callHackage "pandoc" "2.9.2.1"  {})) (old: {
+        executableSystemDepends = [
+          pkgs.texlive.combined.scheme-basic # TODO does this have all the important stuff?
+        ];
+      });
+
       # extra-deps from stack.yaml
       base-noprelude   = hpNew.callHackage "base-noprelude"   "4.13.0.0"  {};
       ConfigFile       = hpNew.callHackage "ConfigFile"       "1.1.4"     {};
@@ -26,7 +32,6 @@ let
       jira-wiki-markup = hpNew.callHackage "jira-wiki-markup" "1.1.4"     {};
       json             = hpNew.callHackage "json"             "0.10"      {};
       MissingH         = hpNew.callHackage "MissingH"         "1.4.3.0"   {};
-      pandoc           = dontCheck (hpNew.callHackage "pandoc" "2.9.2.1"  {}); # TODO what's wrong here?
       pandoc-types     = hpNew.callHackage "pandoc-types"     "1.20"      {};
       recaptcha        = hpNew.callHackage "recaptcha"        "0.1.0.4"   {};
 
@@ -41,7 +46,6 @@ let
   project = overrideCabal haskellPackages.gitit (old: {
     executableSystemDepends = with pkgs; [
       graphviz
-      texlive.combined.scheme-full # TODO is this overkill? it includes every TeX package
       # TODO system-sendmail?
     ];
   });
